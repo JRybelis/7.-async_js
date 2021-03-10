6d5d8bd923e15f8726b848e9f584f21176a81ab5
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-03-10 15:05:00 +0200

    callback nesting

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
-const getTodos = (callback) => {
+const getTodos = (resource, callback) => {
     const request = new XMLHttpRequest();
 
     //reacting to ready state change of the XMLHttp request:
     request.addEventListener('readystatechange', () => {
         if(request.readyState === 4 && request.status === 200) {
-            callback(undefined, request.responseText);            
+            const data = JSON.parse(request.responseText);
+            callback(undefined, data);            
         } else if (request.readyState === 4) {
             callback('Could not fetch the data', undefined);
         }
     });
 
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.open('GET', resource);
     request.send();
 };
 
 //callback function:
-getTodos((error, data) => {
-    if (error) {
-        console.log(error);
-    } else {
+getTodos('todos/simA.json', (error, data) => {
+    // if (error) {
+    //     console.log(error);
+    // } else {
+    //     console.log(data);
+    // }
+    console.log(data);
+    getTodos('todos/simB.json', (error, data) => {
         console.log(data);
-    }
+        getTodos('todos/simC.json', (error, data) => {
+            console.log(data);
+        });
+    });
 });
